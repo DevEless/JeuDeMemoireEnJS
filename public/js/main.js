331889fae6058a498525c68b43a0b181c44fd13a
DevEless <qdelameilleure@gmaile.com>
2023-01-20 11:23:03 +0100

    fin du switch

--- public/js/main.js
+++ public/js/main.js
@@ -18,10 +18,50 @@ function afficherTableau() {
         txt += "<div>";
         
         for(let j=0; j < tabJeu[i].length ; j++){
-        txt += "<button class ='btn btn-primary'>Afficher</button>"
+            if(tabJeu[i][j] === 0){
+
+                txt += "<button class ='btn btn-primary m-2' style='width: 200px; height: 250px'>Afficher</button>"
+            }else{
+                txt += `<img src=  '${getImage(tabJeu[i][j])}' style='width: 200px; height: 250px' class='m-2'>`;
+            }
         }
         txt += "</div>";
     }
     divResultat.innerHTML = txt;
 
+}
+
+function getImage(valeur) {
+    let imgTxt = "";
+    switch (valeur) {
+        case 1: imgTxt += "../../public/image/pig.png";
+            
+            break;
+            case 2:
+                imgTxt += "../../public/image/elephant.png";
+            break;
+            case 3:
+                imgTxt += "../../public/image/giraffe.png";
+            break;
+            case 4:
+                imgTxt += "../../public/image/hippo.png";
+            break;
+            case 5:
+                imgTxt += "../../public/image/monkey.png";
+            break;
+            case 6:
+                imgTxt += "../../public/image/panda.png";
+            break;
+            case 7:
+                imgTxt += "../../public/image/parrot.png";
+            break;
+            case 8:
+                imgTxt += "../../public/image/penguin.png";
+            break;
+    
+        default: console.log("cas non pris en compte");
+            break;
+    }
+    return imgTxt;
+    
 }
