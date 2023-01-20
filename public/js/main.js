6738ad15a34677cddde86268dea10d9cecdd314a
DevEless <qdelameilleure@gmaile.com>
2023-01-20 10:30:25 +0100

    création de la boucle for qui va donner les 4 bouttons

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,27 @@
-let tabJeu = [
+
+const divResultat = document.querySelector('#resultat')
+
+var tabJeu = [
 [0,0,0,0],
 [0,0,0,0],
 [0,0,0,0],
 [0,0,0,0]
 ];
+
+afficherTableau();
+
+function afficherTableau() {
+    let txt = ""
+
+    for(let i=0; i < tabJeu.length; i++){
+        
+        txt += "<div>";
+        
+        for(let j=0; j < tabJeu[i].length ; j++){
+        txt += "<button class ='btn btn-primary'>Afficher</button>"
+        }
+        txt += "</div>";
+    }
+    divResultat.innerHTML = txt;
+
+}
