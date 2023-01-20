b3d2a66cec82c6beb505943e1001918e8e348d6c
DevEless <qdelameilleure@gmaile.com>
2023-01-20 16:07:59 +0100

    background

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,10 @@
 let btn1 = document.querySelectorAll('.btn.btn-secondary')[0]
 let btn2 = document.querySelectorAll('.btn.btn-secondary')[1]
+let madiv = document.getElementById('mode')
+
+btn1.addEventListener("click", () => {
+    console.log('hey je me fous de ta gueule hihi')
+    document.body.innerHTML += "<script src='./public/js/modules/_facile.js'></script>";
+});
+
+
