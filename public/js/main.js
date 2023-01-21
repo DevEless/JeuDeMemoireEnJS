8400ec62a0d2b65913e34a97b455c5b631a6fa1f
DevEless <qdelameilleure@gmaile.com>
2023-01-21 10:52:07 +0100

    mode facile et difficile

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,294 @@
+//VARIABLE
 let btn1 = document.querySelectorAll('.btn.btn-secondary')[0]
 let btn2 = document.querySelectorAll('.btn.btn-secondary')[1]
 let madiv = document.getElementById('mode')
 
-btn1.addEventListener("click", () => {
-    console.log('hey je me fous de ta gueule hihi')
-    document.body.innerHTML += "<script src='./public/js/modules/_facile.js'></script>";
+let timer = 0;
+let interval;
+let started = false;
+let name2 = document.getElementById('fname');
+let startStopButton = document.getElementById("startStop");
+let timeDisplay = document.getElementById("time");
+let nameInput = document.getElementById("name");
+
+
+// VARIABLE FACILE
+const divResultat = document.querySelector('#resultat')
+
+
+
+var tabJeu = [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+
+
+];
+
+
+var tabResultat = genereTableauAleatoire()
+
+let oldselection = [];
+let nbAffiche = 0;
+var ready = true;
+
+
+//TIMER
+function updateTime() {
+    let hours = Math.floor(timer / 3600);
+    let minutes = Math.floor((timer % 3600) / 60);
+    let seconds = timer % 60;
+
+    timeDisplay.innerHTML = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+}
+
+startStopButton.addEventListener("click", () => {
+    if (!started) {
+        interval = setInterval(() => {
+            timer++;
+            updateTime();
+        }, 1000);
+        started = true;
+        name2.textContent = nameInput.value
+        startStopButton.innerHTML = "Stop";
+    } else {
+        clearInterval(interval);
+        started = false;
+        startStopButton.innerHTML = "Start";
+    }
 });
 
 
+
+//FACILE MODE 
+
+btn1.addEventListener("click", afficherTableau)
+
+function afficherTableau() {
+    let txt = ""
+
+    for (let i = 0; i < tabJeu.length; i++) {
+
+        txt += "<div>";
+
+        for (let j = 0; j < tabJeu[i].length; j++) {
+            if (tabJeu[i][j] === 0) {
+
+                txt += "<button class ='btn btn-dark m-2' style='width: 130px; height: 130px' onClick='verif(\"" + i + "-" + j + "\")'></button> ";
+
+            } else {
+                txt += `<img src=  '${getImage(tabJeu[i][j])}' style='width: 130px; height: 130px' class='m-2'>`;
+            }
+        }
+        txt += "</div>";
+    }
+    divResultat.innerHTML = txt;
+
+}
+
+function getImage(valeur) {
+    let imgTxt = "";
+    switch (valeur) {
+        case 1:
+            imgTxt += "../../public/image/un.png";
+
+            break;
+        case 2:
+            imgTxt += "../../public/image/deux.png";
+            break;
+        case 3:
+            imgTxt += "../../public/image/trois.png";
+            break;
+
+
+        default:
+            console.log("cas non pris en compte");
+            break;
+    }
+    return imgTxt;
+
+}
+
+
+function verif(bouton) {
+    if (ready) {
+
+        nbAffiche++;
+
+        var ligne = bouton.substr(0, 1);
+        var colonne = bouton.substr(2, 1);
+        console.log(ligne);
+        console.log(colonne);
+        tabJeu[ligne][colonne] = tabResultat[ligne][colonne];
+        afficherTableau();
+
+        if (nbAffiche > 1) {
+            ready = false;
+            setTimeout(() => {
+                if (tabJeu[ligne][colonne] !== tabResultat[oldselection[0]][oldselection[1]]) {
+                    tabJeu[ligne][colonne] = 0;
+                    tabJeu[oldselection[0]][oldselection[1]] = 0;
+                }
+                afficherTableau()
+                ready = true
+
+                nbAffiche = 0;
+            }, 1000);
+        } else {
+            oldselection = [ligne, colonne];
+        }
+    }
+}
+
+function genereTableauAleatoire() {
+    var tab = [];
+    var nbImagePosition = [0, 0, 0]
+
+    for (let i = 0; i < 3; i++) {
+        let ligne = [];
+        for (let j = 0; j < 2; j++) {
+            var fin = false;
+            while (!fin) {
+                var randomImage = Math.floor(Math.random() * 3);
+                if (nbImagePosition[randomImage] < 2) {
+                    ligne.push(randomImage + 1);
+                    nbImagePosition[randomImage]++;
+                    fin = true;
+                }
+            }
+
+        }
+
+        tab.push(ligne);
+    }
+    return tab;
+}
+
+//DIFFICILE MODE
+
+btn2.addEventListener("click", afficherTableau1)
+var tabJeu1 = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+];
+
+
+var tabResultat1 = genereTableauAleatoire1()
+
+
+
+function afficherTableau1() {
+    let txt = ""
+
+    for (let i = 0; i < tabJeu1.length; i++) {
+
+        txt += "<div>";
+
+        for (let j = 0; j < tabJeu1[i].length; j++) {
+            if (tabJeu1[i][j] === 0) {
+
+                txt += "<button class ='btn btn-dark m-2' style='width: 130px; height: 130px' onClick='verif1(\"" + i + "-" + j + "\")'></button> ";
+
+            } else {
+                txt += `<img src=  '${getImage1(tabJeu1[i][j])}' style='width: 130px; height: 130px' class='m-2'>`;
+            }
+        }
+        txt += "</div>";
+    }
+    divResultat.innerHTML = txt;
+
+}
+
+function getImage1(valeur) {
+    let imgTxt = "";
+    switch (valeur) {
+        case 1:
+            imgTxt += "../../public/image/un.png";
+
+            break;
+        case 2:
+            imgTxt += "../../public/image/deux.png";
+            break;
+        case 3:
+            imgTxt += "../../public/image/trois.png";
+            break;
+        case 4:
+            imgTxt += "../../public/image/quatre.png";
+            break;
+        case 5:
+            imgTxt += "../../public/image/cinq.png";
+            break;
+        case 6:
+            imgTxt += "../../public/image/six.png";
+            break;
+        case 7:
+            imgTxt += "../../public/image/sept.png";
+            break;
+        case 8:
+            imgTxt += "../../public/image/huit.png";
+            break;
+
+        default:
+            console.log("cas non pris en compte");
+            break;
+    }
+    return imgTxt;
+
+}
+
+
+function verif1(bouton) {
+    if (ready) {
+
+        nbAffiche++;
+
+        var ligne = bouton.substr(0, 1);
+        var colonne = bouton.substr(2, 1);
+        console.log(ligne);
+        console.log(colonne);
+        tabJeu1[ligne][colonne] = tabResultat1[ligne][colonne];
+        afficherTableau1();
+
+        if (nbAffiche > 1) {
+            ready = false;
+            setTimeout(() => {
+                if (tabJeu1[ligne][colonne] !== tabResultat1[oldselection[0]][oldselection[1]]) {
+                    tabJeu1[ligne][colonne] = 0;
+                    tabJeu1[oldselection[0]][oldselection[1]] = 0;
+                }
+                afficherTableau1()
+                ready = true
+
+                nbAffiche = 0;
+            }, 1000);
+        } else {
+            oldselection = [ligne, colonne];
+        }
+    }
+}
+
+function genereTableauAleatoire1() {
+    var tab = [];
+    var nbImagePosition = [0, 0, 0, 0, 0, 0, 0, 0]
+
+    for (let i = 0; i < 4; i++) {
+        let ligne = [];
+        for (let j = 0; j < 4; j++) {
+            var fin = false;
+            while (!fin) {
+                var randomImage = Math.floor(Math.random() * 8);
+                if (nbImagePosition[randomImage] < 2) {
+                    ligne.push(randomImage + 1);
+                    nbImagePosition[randomImage]++;
+                    fin = true;
+                }
+            }
+
+        }
+
+        tab.push(ligne);
+    }
+    return tab;
+}
